88eb55e00649bc9eddf9059c3a7a37fe9bbb4bbe
aryandev345 <122824496+aryandev345@users.noreply.github.com>
2024-06-05 21:51:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('box').addEventListener('click', function() {
     this.classList.add('clicked');
     this.innerHTML = `
-        <p id="message"><strong>Happy Birthday My Love!</strong></p>
+        <p id="message"><strong>Happy Birthday My Love! I cant express in words my love for you you are that special one in my life i promised myself that i will gift you flowers on your birthday and i hope i kept my promise!!! From the day i met you, you have captivated me to the fullest no other person comes close to my heart than you. i know i say this a lot but again i love you and a very happy birthday!!!!!!</strong></p>
         <img id="flowerImage" src="flower.jpg" alt="Flowers" class="show">
     `;
     playBackgroundMusic();
